6db6dea85f09bcf6e9a4667c717b57d601f6f366
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-09-29 00:58:07 -0400

    Clean up tests

--- fs_test.go
+++ fs_test.go
@@ -6,12 +6,12 @@ import "github.com/nowk/assert"
 
 func TestFSPathError(t *testing.T) {
 	name, v := FS("path/to/doesnotexist.txt")
-	r := v.(io.ReadCloser)
-	defer r.Close()
-
 	assert.Equal(t, "doesnotexist.txt.txt", name)
 
 	b := make([]byte, 32*1024)
+	r := v.(io.ReadCloser)
 	n, _ := r.Read(b)
 	assert.Equal(t, "open path/to/doesnotexist.txt: no such file or directory", string(b[:n]))
+
+	r.Close()
 }
--- http_test.go
+++ http_test.go
@@ -6,31 +6,31 @@ import "testing"
 import "github.com/nowk/assert"
 
 func TestHTTPURLParseError(t *testing.T) {
-	badURL := "thisisabadurl"
-	name, v := HTTP(badURL)
-	r := v.(io.ReadCloser)
-	defer r.Close()
-
+	name, v := HTTP("thisisabadurl")
 	assert.Equal(t, "thisisabadurl.txt", name)
 
 	b := make([]byte, 32*1024)
+	r := v.(io.ReadCloser)
 	n, _ := r.Read(b)
 	assert.Equal(t, "parse thisisabadurl: invalid URI for request", string(b[:n]))
+
+	r.Close()
 }
 
 func TestHTTPClientError(t *testing.T) {
-	name, v := HTTP("http://unreachable") // fails if ISP picks up and redirects to search, which TWC does
-	r := v.(io.ReadCloser)
-	defer r.Close()
-
+	// fails if ISP picks up and redirects to search, which TWC does
+	name, v := HTTP("http://unreachable")
 	assert.Equal(t, "unreachable.txt", name)
 
 	reg := regexp.MustCompile(`Get http:\/\/unreachable:( dial tcp:)? lookup unreachable: no such host`)
 	b := make([]byte, 32*1024)
+	r := v.(io.ReadCloser)
 	n, _ := r.Read(b)
 	if str := string(b[:n]); !reg.MatchString(str) {
 		t.Errorf("Expected %s, got %s", reg.String(), str)
 	}
+
+	r.Close()
 }
 
 // func TestHTTPAppendsExtFromContentType(t *testing.T) {
