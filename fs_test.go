d398cba5b69e4993709d1682d31b85fac9af9cab
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-10-01 00:50:46 -0400

    Remove type assertion

--- fs_test.go
+++ fs_test.go
@@ -1,19 +1,16 @@
 package sources
 
-import "io"
 import "testing"
 import "github.com/nowk/assert"
 
 func TestFSPathError(t *testing.T) {
 	errmsg := "open path/to/doesnotexist.txt: no such file or directory"
-	name, v, err := FS("path/to/doesnotexist.txt")
+	name, r, err := FS("path/to/doesnotexist.txt")
+	defer r.Close()
 	assert.Equal(t, "doesnotexist.txt.txt", name)
 	assert.Equal(t, errmsg, err.Error())
 
 	b := make([]byte, 32*1024)
-	r := v.(io.ReadCloser)
 	n, _ := r.Read(b)
 	assert.Equal(t, errmsg, string(b[:n]))
-
-	r.Close()
 }
--- http_test.go
+++ http_test.go
@@ -1,6 +1,5 @@
 package sources
 
-import "io"
 import "regexp"
 import "testing"
 import "github.com/nowk/assert"
@@ -8,36 +7,34 @@ import "github.com/nowk/assert"
 func TestHTTPURLParseError(t *testing.T) {
 	errmsg := "parse thisisabadurl: invalid URI for request"
 
-	name, v, err := HTTP("thisisabadurl")
+	name, r, err := HTTP("thisisabadurl")
+	defer r.Close()
+
 	assert.Equal(t, "thisisabadurl.txt", name)
 	assert.Equal(t, errmsg, err.Error())
 
 	b := make([]byte, 32*1024)
-	r := v.(io.ReadCloser)
 	n, _ := r.Read(b)
 	assert.Equal(t, errmsg, string(b[:n]))
-
-	r.Close()
 }
 
 func TestHTTPClientError(t *testing.T) {
 	reg := regexp.MustCompile(`Get http:\/\/unreachable:( dial tcp:)? lookup unreachable: no such host`)
 
-	// fails if ISP picks up and redirects to search, which TWC does
-	name, v, err := HTTP("http://unreachable")
+	// NOTE fails if ISP picks up and redirects to search, which TWC does
+	name, r, err := HTTP("http://unreachable")
+	defer r.Close()
+
 	assert.Equal(t, "unreachable.txt", name)
 	if !reg.MatchString(err.Error()) {
 		t.Errorf("Expected %s to match %s", err.Error(), reg.String())
 	}
 
 	b := make([]byte, 32*1024)
-	r := v.(io.ReadCloser)
 	n, _ := r.Read(b)
 	if str := string(b[:n]); !reg.MatchString(str) {
 		t.Errorf("Expected %s to match %s", str, reg.String())
 	}
-
-	r.Close()
 }
 
 // func TestHTTPAppendsExtFromContentType(t *testing.T) {
